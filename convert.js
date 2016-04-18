574f5d98c706e6bd03f1bb3d2a2ed95253945c76
1000ch <shogo.sensui@gmail.com>
2016-04-18 19:04:16 +0900

    Move images

--- convert.js
+++ convert.js
@@ -3,15 +3,16 @@ const path = require('path');
 const execFile = require('child_process').execFile;
 const cwebp = require('cwebp-bin');
 
-fs.readdirSync('src/img').forEach(file => {
+fs.readdirSync('src/img/tani').forEach(file => {
   const extension = path.extname(file);
   const basename = path.basename(file, extension);
-  const input = `src/img/${file}`;
-  const output = `dist/img/${basename}.webp`;
+  const input = `src/img/tani/${file}`;
+  const output = `dist/img/tani/${basename}.webp`;
   execFile(cwebp, [input, '-o', output],  error => {
     if (error) {
       console.error(error);
+    } else {
+      console.log(`${input} is converted into ${output}!`);
     }
-    console.log(`${input} is converted into ${output}!`);
   });
 });

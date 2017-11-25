7800406e6782b9c46fe1555cab6160bfc2824929
1000ch <shogo.sensui@gmail.com>
2017-11-26 01:34:24 +0900

    Drop Rx and dom-delegate

--- src/app.js
+++ src/app.js
@@ -4,9 +4,6 @@ if (navigator.serviceWorker) {
     .catch(error => console.error(error));
 }
 
-import Rx from 'rx';
-import Delegate from 'dom-delegate';
-
 const tanis = [
   { id : '78d2f3a9' },
   { id : '95825320' },
@@ -68,105 +65,101 @@ const affiliates = [
   }
 ];
 
-Rx.Observable
-  .fromEvent(document, 'DOMContentLoaded')
-  .subscribe(() => {
-    const imageContainer = document.querySelector('#image-container');
-    const affiliateContainer = document.querySelector('#affiliate-container');
-    const backdrop = document.querySelector('#backdrop');
-    const ids = tanis.map(tani => tani.id);
+document.addEventListener('DOMContentLoaded', () => {
+  const imageContainer = document.querySelector('#image-container');
+  const affiliateContainer = document.querySelector('#affiliate-container');
+  const backdrop = document.querySelector('#backdrop');
+  const ids = tanis.map(tani => tani.id);
 
-    for (let tani of tanis) {
-      let cell = document.createElement('div');
-      cell.className = 'Cell -4of12';
-      let item = document.createElement('tani-image');
-      item.id = tani.id;
-      cell.appendChild(item);
-      imageContainer.appendChild(cell);
+  for (let tani of tanis) {
+    let cell = document.createElement('div');
+    cell.className = 'Cell -4of12';
+    let item = document.createElement('tani-image');
+    item.id = tani.id;
+    cell.appendChild(item);
+    imageContainer.appendChild(cell);
+  }
+
+  for (let affiliate of affiliates) {
+    let cell = document.createElement('div');
+    cell.className = 'Cell -4of12';
+    let item = document.createElement('book-affiliate');
+    item.setAttribute('url', affiliate.url);
+    item.setAttribute('name', affiliate.name);
+    item.setAttribute('title', affiliate.title);
+    cell.appendChild(item);
+    affiliateContainer.appendChild(cell);
+  }
+
+  const image = backdrop.querySelector('img');
+  const images = imageContainer.querySelectorAll('tani-image');
+
+  backdrop.addEventListener('click', e => {
+    if (e.target.tagName.toLowerCase() === 'img') {
+      return;
     }
 
-    for (let affiliate of affiliates) {
-      let cell = document.createElement('div');
-      cell.className = 'Cell -4of12';
-      let item = document.createElement('book-affiliate');
-      item.setAttribute('url', affiliate.url);
-      item.setAttribute('name', affiliate.name);
-      item.setAttribute('title', affiliate.title);
-      cell.appendChild(item);
-      affiliateContainer.appendChild(cell);
-    }
+    backdrop.classList.add('Backdrop--hidden');
+    history.pushState(null, null, '#');
+  });
 
-    const image = backdrop.querySelector('img');
-    const images = imageContainer.querySelectorAll('tani-image');
+  imageContainer.addEventListener('click', e => {
+    backdrop.classList.remove('Backdrop--hidden');
+    image.src = e.target.src;
+    location.hash = e.target.id;
+  });
 
-    Rx.Observable
-      .fromEvent(backdrop, 'click')
-      .filter(e => e.target.tagName.toLowerCase() !== 'img')
-      .subscribe(() => {
-        backdrop.classList.add('Backdrop--hidden');
-        history.pushState(null, null, '#');
-      });
-
-    const delegate = new Delegate(imageContainer);
-
-    Rx.Observable
-      .fromEvent(delegate, 'click')
-      .subscribe(e => {
-        backdrop.classList.remove('Backdrop--hidden');
-        image.src = e.target.src;
-        location.hash = e.target.id;
-      });
-
-    Rx.Observable
-      .fromEvent(window, 'hashchange')
-      .filter(e => {
-        const targetId = location.hash.replace('#', '');
-        const targetIndex = ids.indexOf(targetId);
-        return targetId !== '' && targetIndex !== -1;
-      })
-      .subscribe(e => {
-        backdrop.classList.remove('Backdrop--hidden');
-        const targetId = location.hash.replace('#', '');
-        const targetIndex = ids.indexOf(targetId);
-        image.dataset.id = images[targetIndex].id;
-        image.src = images[targetIndex].src;
-      });
-
-    const KEYCODE_ESC = 27;
-    const KEYCODE_LEFTARROW = 37;
-    const KEYCODE_RIGHTARROW = 39;
-
-    const keydown = Rx.Observable
-      .fromEvent(document, 'keydown')
-      .filter(e => !backdrop.classList.contains('Backdrop--hidden'));
-
-    keydown
-      .filter(e => e.keyCode === KEYCODE_ESC)
-      .subscribe(() => {
-        backdrop.classList.add('Backdrop--hidden');
-        history.pushState(null, null, '#');
-      });
-
-    keydown
-      .filter(e => e.keyCode === KEYCODE_LEFTARROW)
-      .filter(() => ids.indexOf(image.dataset.id) !== 0)
-      .subscribe(() => {
-        const index = ids.indexOf(image.dataset.id);
-        location.hash = ids[index - 1];
-      });
-
-    keydown
-      .filter(e => e.keyCode === KEYCODE_RIGHTARROW)
-      .filter(() => ids.indexOf(image.dataset.id) !== ids.length - 1)
-      .subscribe(() => {
-        const index = ids.indexOf(image.dataset.id);
-        location.hash = ids[index + 1];
-      });
-
+  window.addEventListener('hashchange', () => {
     const targetId = location.hash.replace('#', '');
-    for (let image of images) {
-      if (image.id === targetId) {
-        image.click();
-      }
+    if (targetId === '') {
+      return;
+    }
+
+    const targetIndex = ids.indexOf(targetId);
+    if (targetIndex === -1) {
+      return;
+    }
+
+    backdrop.classList.remove('Backdrop--hidden');
+    image.dataset.id = images[targetIndex].id;
+    image.src = images[targetIndex].src;
+  });
+
+  const KEYCODE_ESC = 27;
+  const KEYCODE_LEFTARROW = 37;
+  const KEYCODE_RIGHTARROW = 39;
+
+  document.addEventListener('keydown', e => {
+    if (backdrop.classList.contains('Backdrop--hidden')) {
+      return;
+    }
+
+    switch (e.keyCode) {
+      case KEYCODE_ESC:
+        backdrop.classList.add('Backdrop--hidden');
+        history.pushState(null, null, '#');
+        break;
+      case KEYCODE_LEFTARROW:
+        if (ids.indexOf(image.dataset.id) === 0) {
+          return;
+        }
+
+        location.hash = ids[ids.indexOf(image.dataset.id) - 1];
+        break;
+      case KEYCODE_RIGHTARROW:
+        if (ids.indexOf(image.dataset.id) === ids.length - 1) {
+          return;
+        }
+
+        location.hash = ids[ids.indexOf(image.dataset.id) + 1];
+        break;
     }
   });
+
+  const targetId = location.hash.replace('#', '');
+  for (let image of images) {
+    if (image.id === targetId) {
+      image.click();
+    }
+  }
+});
